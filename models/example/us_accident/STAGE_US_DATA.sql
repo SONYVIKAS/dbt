bb23cb54294055ba75c764a1b5987fbdca57cbae
unknown <sonyvikas>
2022-12-06 11:00:35 +0530

    hi

---
+++ models/example/us_accident/STAGE_US_DATA.sql
@@ -0,0 +1,6 @@
+{{ config(post_hook="truncate table {{source('DEVELOPER_DB','US_ACCIDENTS_DATA_RAW_STAGE')}}",
+materialized='table') }}
+select to_number(to_char(START_TIME, 'yyyymmdd')) as date_id,* from {{source('DEVELOPER_DB','US_ACCIDENTS_DATA_RAW_STAGE')}}
+
+
+
---
+++ models/example/us_accident/US_ACCIDENTS_ADRESS_DIM.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='incremental',unique_key='ADD_ID') }}
+with example_2 as
+(select distinct CITY,_MODIFIED,COUNTRY, STATE, ZIPCODE, COUNTY, TIMEZONE, AIRPORT_CODE, STREET from {{source('DEVELOPER_DB','STAGE_US_DATA')}}
+),
+source_data1 as(
+select concat('A-', cast(seq.nextval as varchar)) as ADD_ID,
+CITY, COUNTRY, STATE, ZIPCODE, COUNTY, TIMEZONE, AIRPORT_CODE, STREET , _MODIFIED as LOAD_TIME from example_2 order by ADD_ID
+)
+select ADD_ID,CITY, COUNTRY, STATE, ZIPCODE, COUNTY, TIMEZONE, AIRPORT_CODE, STREET, LOAD_TIME from source_data1
